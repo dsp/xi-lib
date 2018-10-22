7a8e135244bbffc3836120e27620cb23c03490ea
David Soria Parra <dsp@experimentalworks.net>
2018-10-21 18:06:58 -0700

    Introduce an external handle and an internal state
    We are introducing an internal state, which is handled by a private
    struct. This allows us to keep implementation specific details to the
    library and force callees to go through the public interface.
    
    We are using a new struct XiHandle to the user to identify a specific
    instance, which can be created using xi_init().

--- src/lib.rs
+++ src/lib.rs
@@ -11,20 +11,46 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+extern crate xi_core_lib;
+
+use std::os::raw::c_char;
+
+use xi_core_lib::{XiCore};
+
+/// We are keeping our internal state in a private struct and only expose
+/// a simple handler to callees. Callees must go through the public interface
+/// to interact with internals.
+struct XiInternalState {
+    core: XiCore,
+}
+
+impl XiInternalState {
+    fn new() -> Self {
+        XiInternalState {
+            core: XiCore::new(),
+        }
+    }
+}
 
 #[repr(C)]
-pub struct XiCore {
+pub struct XiHandle {
     version: u32,
+    internal: XiInternalState,
 }
 
 #[no_mangle]
-pub extern "C" fn xi_init() -> XiCore {
-    XiCore {
+pub extern "C" fn xi_init(xi: &XiHandle) -> XiHandle {
+    XiHandle {
         version: 1,
+        internal: XiInternalState::new(),
     }
 }
 
 #[no_mangle]
+pub extern "C" fn xi_send_message(xi: XiHandle, msg: *const c_char) {
+
+}
+
+#[no_mangle]
 pub extern "C" fn xi_shutdown() {
-    println!("hello world");
 }
