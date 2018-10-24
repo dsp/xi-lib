601092b6ca987c017576cd0968e78e22727cd90f
David Soria Parra <dsp@experimentalworks.net>
2018-10-23 20:16:18 -0700

    Add a pthread example of our xi-lib

--- tests/init.c
+++ tests/init.c
@@ -1,15 +1,26 @@
 #include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
 #include <xilib.h>
 
 void callme(const char* msg, uint32_t len) {
     printf("%s\n", msg);
 }
 
-int main(int argc, char ** argv) {
-    XiHandle * xi = xi_create(callme);
+void * xi_thread(void *user_data) {
+    XiHandle *xi = (XiHandle *) user_data;
     xi_start(xi);
-    xi_send_message(xi, "foo", sizeof("foo"));
+    usleep(50000); // 50 ms
     xi_shutdown(xi);
+    return NULL;
+}
+
+int main(int argc, char ** argv) {
+    pthread_t xi_thread_id;
+    XiHandle * xi = xi_create(callme);
+    pthread_create(&xi_thread_id, NULL, xi_thread, (void *) xi);
+    xi_send_message(xi, "foo", sizeof("foo"));
+    pthread_join(xi_thread_id, NULL);
     xi_free(xi);
     return 0;
 }
