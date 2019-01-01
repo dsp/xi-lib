ff8771de1312c176c9a85c1f73dfa2e3b9bc1796
David Soria Parra <dsp@experimentalworks.net>
2018-12-31 18:08:55 -0800

    examples: change example to use a core and receiver threads.

--- tests/init.c
+++ tests/init.c
@@ -2,25 +2,40 @@
 #include <pthread.h>
 #include <unistd.h>
 #include <xilib.h>
+#include <string.h>
 
 void callme(const char* msg, uint32_t len) {
-    printf("%s\n", msg);
+    printf("received: %s\n", msg);
 }
 
-void * xi_thread(void *user_data) {
+void * xi_receiver_thread(void *user_data) {
     XiHandle *xi = (XiHandle *) user_data;
-    xi_start(xi);
-    usleep(50000); // 50 ms
-    xi_shutdown(xi);
+    xi_start_receiver(xi);
+    return NULL;
+}
+
+void * xi_core_thread(void *user_data) {
+    XiHandle *xi = (XiHandle *) user_data;
+    xi_start_core(xi);
     return NULL;
 }
 
 int main(int argc, char ** argv) {
-    pthread_t xi_thread_id;
+    pthread_t xi_core_thread_id, xi_receiver_thread_id;
     XiHandle * xi = xi_create(callme);
-    pthread_create(&xi_thread_id, NULL, xi_thread, (void *) xi);
-    xi_send_message(xi, "{\"method\": \"new_view\"}", sizeof("{\"method\": \"new_view\"}"));
-    pthread_join(xi_thread_id, NULL);
+
+    pthread_create(&xi_core_thread_id, NULL, xi_core_thread, (void *) xi);
+    pthread_create(&xi_receiver_thread_id, NULL, xi_receiver_thread, (void *) xi);
+
+    const char * msg = "{\"method\": \"client_started\", \"params\": {\"config_dir\":null, \"client_extras_dir\": null}}";
+    xi_send_message(xi, msg, strlen(msg) + 1);
+    
+    const char * msg2 = "{\"id\": 1, \"method\": \"new_view\", \"params\": {\"file_path\": null}}";
+    xi_send_message(xi, msg2, strlen(msg2) + 1);
+
+    pthread_join(xi_receiver_thread_id, NULL);
+    pthread_join(xi_core_thread_id, NULL);
+
     xi_free(xi);
     return 0;
 }
